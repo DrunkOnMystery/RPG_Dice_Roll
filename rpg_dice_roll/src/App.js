16888bfdf5678a1f5aa3affd7081cbd4726423b5
Ben Brumby <bbrumby00@gmail.com>
2021-05-05 23:30:11 -0400

    added die list

--- rpg_dice_roll/src/App.js
+++ rpg_dice_roll/src/App.js
@@ -8,6 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>RPG DICE ROLLER APP</h1>
+          <p>The goal of this app is to create an app that can be used to replace a typical set of RPG dice.</p>
+          <p>D20</p> <br />
+          <p>D12</p> <br />
+          <p>D10</p> <br />
+          <p>D8</p> <br />
+          <p>D6</p> <br />
+          <p>D4</p> <br />
         </header>
       </div>
     );
